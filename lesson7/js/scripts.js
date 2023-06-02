c07f94aa5fd68ef210f11141f88c2f176052724d
ulises-u <ulisesmartir@byui.edu>
2023-06-02 01:48:26 -0500

    Progressive Loading of Images

---
+++ lesson7/js/scripts.js
@@ -0,0 +1,39 @@
+document.getElementById("modify").innerHTML = document.lastModified
+document.getElementById("year").innerHTML = new Date().getFullYear();
+
+
+/*--------------lazyload---------------*/
+const allImages = document.querySelectorAll("img[data-src]")
+
+const lazyLoad = (img) => {
+    img.setAttribute("src", img.getAttribute("data-src"))
+    img.onload = () => {
+        img.removeAttribute("data-src")
+        img.className = "in"
+    };
+};
+
+
+const options = {
+    threshold: 0,
+    rootMargin: "0px 0px 50px 0px"
+}
+
+if ('IntersectionObserver' in window) {
+    const obsrvr = new IntersectionObserver((items, observer) => {
+        items.forEach((item) => {
+            if(item.isIntersecting) {
+                lazyLoad(item.target)
+                observer.unobserve(item.target)
+            }
+        })
+    }, options)
+    allImages.forEach((img) => {
+        obsrvr.observe(img)
+    })
+}
+else {
+    allImages.forEach((img) => {
+        lazyLoad(img)
+    })
+}
