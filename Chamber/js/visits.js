8b2b9de3e279dd9fe706d609d020e1dfd2702540
ulises-u <ulisesmartir@byui.edu>
2023-06-06 23:38:30 -0500

    number of visit

--- Chamber/js/visits.js
+++ Chamber/js/visits.js
@@ -1,9 +1,12 @@
 "use strict";
 
+
+
+
 let numOfVisits = document.querySelector('.daysOfVisit');
 
 let numOfVisits1 = Number(window.localStorage.getItem('visits'));
-let lastVisits= Number(window.localStorage.getItem('lastVisits'));
+let lastVisits = Number(window.localStorage.getItem('lastVisits'));
 
 const NUMS = 1000 * 60 * 60 * 24;
 
@@ -13,12 +16,11 @@ let numOfDays = Math.ceil(daysAmong / NUMS);
 
 localStorage.setItem('lastVisits', Date.now());
 
-if (numOfVisits1 != 0) {
-
-    numOfVisits.textContent = 'It\'s been ' + numOfDays + ' day(s) since your last visit.'
+if (numOfVisits1 !== 0) {
+  numOfVisits.textContent = 'You have visited ' + numOfVisits1 + ' time(s).';
 } else {
-    numOfVisits.textContent = 'This is your first page visit.'
+  numOfVisits.textContent = 'This is your first page visit.';
 }
 
 numOfVisits1++;
-localStorage.setItem("visits", numOfVisits1);
+localStorage.setItem('visits', numOfVisits1);
--- Chamber/js/windchill.js
+++ Chamber/js/windchill.js
@@ -1,16 +1,44 @@
 
-"use strict";
 
-const speed = parseFloat(document.querySelector("#speed").textContent);
-const temp = parseFloat(document.querySelector("#tempurature").textContent);
-const f = 35.74 + 0.6215 * temp - 35.75 * speed ** 0.16 + 0.4275 * temp * speed ** 0.16
-if (temp <= 50 && speed > 3) {
-windchill= f.toFixed(1);
-}
-else {
-    windchill = "N/A";
-}
-document.getElementById("windchill").innerHTML = windchill;
+// Declare and assign the API URL
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=4903832&appid=caa8540702ef690bc84e562267149524&units=imperial';
 
+// Fetch weather data from the API
+fetch(apiURL)
+
+    .then((response) => response.json())
+    .then((jsonObject) => {
+        console.log(jsonObject);
+
+        const iconURL = `https://openweathermap.org/img/w/${jsonObject.weather[0].icon}.png`;
+
+        let temp = jsonObject.main.temp;
+        let speed = jsonObject.wind.speed;
+
+        document.querySelector('.cityName').textContent = jsonObject.name;
+        document.querySelector('figcaption').textContent = jsonObject.weather[0].description;
+        document.querySelector('#weatherIcon').setAttribute('src', iconURL);
+        document.querySelector('#weatherIcon').setAttribute('alt', jsonObject.weather[0].description);
+        document.querySelector('.temp').innerHTML = `Temperature: ${temp} &deg;F`;
+        document.querySelector('.wind-speed').textContent = `Wind Speed: ${speed} mph`;
+        document.querySelector('.humidityDiv').textContent = `Humidity: ${jsonObject.main.humidity}`;
+
+        let windChill = computeWindChill(temp, speed);
+
+        document.querySelector('.wind-chill').textContent = `Wind Chill: ${windChill}`;
+
+        
+    });
+
+    function computeWindChill(t, s) {
+        windchill = Math.round((35.74 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * t * s ** 0.16) * 100 / 100);
+
+        if (t <= 50 && s > 3) {
+            return windchill
+        } else {
+            return `N/A`
+        }
+    
+    
 
 
