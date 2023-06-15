0fe0d9ddcf205d3182c5bb5d6c9f2a94e6ad5ff7
ulises-u <ulisesmartir@byui.edu>
2023-06-14 21:41:19 -0500

    join fixed

--- Chamber/js/lazy-img.js
+++ Chamber/js/lazy-img.js
@@ -1,33 +1,37 @@
+
+
+/*---------------*/
 "use strict";
 
 let imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const imageOptions = {
-    threshold: 3,
-    rootMargin: '0px 0px 50px 0px'
+  threshold: 0.5,
+  rootMargin: '0px 0px 100px 0px'
 };
 
 const loadImages = (img) => {
-    img.setAttribute('src', img.getAttribute('data-src'));
-    img.onload = () => {
-        img.removeAttribute('data-src');
-    };
+  img.setAttribute('src', img.getAttribute('data-src'));
+  img.onload = () => {
+    img.removeAttribute('data-src');
+  };
 };
 
 if ('IntersectionObserver' in window) {
-    const observer = new IntersectionObserver((items, observer)  => {
-        items.forEach((item) => {
-            if (item.isIntersecting) {
-                loadImages(item.target);
-                observer.unobserve(item.target);
-            }
-        });
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
     });
-    imagesToLoad.forEach((img) => {
-        observer.observe(img);
-    });
+  }, imageOptions);
+  
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
 } else {
-    imagesToLoad.forEach((img) => {
-        loadImages(img);
-    });
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
 }
--- Chamber/js/windchill.js
+++ Chamber/js/windchill.js
@@ -1,44 +1,44 @@
-
-
-// Declare and assign the API URL
-const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=4903832&appid=caa8540702ef690bc84e562267149524&units=imperial';
-
-// Fetch weather data from the API
-fetch(apiURL)
-
-    .then((response) => response.json())
-    .then((jsonObject) => {
-        console.log(jsonObject);
-
-        const iconURL = `https://openweathermap.org/img/w/${jsonObject.weather[0].icon}.png`;
-
-        let temp = jsonObject.main.temp;
-        let speed = jsonObject.wind.speed;
-
-        document.querySelector('.cityName').textContent = jsonObject.name;
-        document.querySelector('figcaption').textContent = jsonObject.weather[0].description;
-        document.querySelector('#weatherIcon').setAttribute('src', iconURL);
-        document.querySelector('#weatherIcon').setAttribute('alt', jsonObject.weather[0].description);
-        document.querySelector('.temp').innerHTML = `Temperature: ${temp} &deg;F`;
-        document.querySelector('.wind-speed').textContent = `Wind Speed: ${speed} mph`;
-        document.querySelector('.humidityDiv').textContent = `Humidity: ${jsonObject.main.humidity}`;
-
-        let windChill = computeWindChill(temp, speed);
-
-        document.querySelector('.wind-chill').textContent = `Wind Chill: ${windChill}`;
-
-        
-    });
-
-    function computeWindChill(t, s) {
-        windchill = Math.round((35.74 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * t * s ** 0.16) * 100 / 100);
-
-        if (t <= 50 && s > 3) {
-            return windchill
-        } else {
-            return `N/A`
-        }
-    
-    
-
-
+function fetchWeatherData() {
+    // Declare and assign the API URL for Nauvoo, IL
+    const apiURL = 'https://api.openweathermap.org/data/2.5/weather?q=Nauvoo,us&appid=YOUR_API_KEY&units=imperial';
+  
+    // Fetch weather data from the API
+    fetch(apiURL)
+      .then((response) => response.json())
+      .then((jsonObject) => {
+        // Display the weather data on the HTML page
+        displayWeatherData(jsonObject);
+      })
+      .catch((error) => {
+        console.log('Error:', error);
+      });
+  }
+  
+  function displayWeatherData(data) {
+    const iconURL = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+  
+    let temp = data.main.temp;
+    let speed = data.wind.speed;
+  
+    document.getElementById('cityName').textContent = data.name;
+    document.getElementById('weatherIcon').setAttribute('src', iconURL);
+    document.getElementById('weatherIcon').setAttribute('alt', data.weather[0].description);
+    document.querySelector('.temp').innerHTML = `Temperature: ${temp} &deg;F`;
+    document.querySelector('.wind-speed').textContent = `Wind Speed: ${speed} mph`;
+    document.querySelector('.humidityDiv').textContent = `Humidity: ${data.main.humidity}`;
+  
+    let windChill = computeWindChill(temp, speed);
+    document.querySelector('.wind-chill').textContent = `Wind Chill: ${windChill}`;
+  }
+  
+  function computeWindChill(t, s) {
+    if (t <= 50 && s > 3) {
+      return Math.round((35.74 + 0.6215 * t - 35.75 * Math.pow(s, 0.16) + 0.4275 * t * Math.pow(s, 0.16)) * 100) / 100;
+    } else {
+      return 'N/A';
+    }
+  }
+  
+  // Call the fetchWeatherData function to retrieve weather data and display it
+  fetchWeatherData();
+ 
