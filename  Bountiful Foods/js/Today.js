478572b8a401903e1d4dc6b1410fa14ea0fa45a3
ulises-u <ulisesmartir@byui.edu>
2023-07-15 23:10:18 -0500

    bountiful foods

---  Bountiful Foods/js/Today.js
+++  Bountiful Foods/js/Today.js
@@ -1,13 +1,26 @@
+
 "use strict";
 
 document.querySelector('#now').innerHTML = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format();
+//document.getElementById("displayDateTime").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
+
+let chamber = document.querySelector('#Bountiful Foods');
+let dayOfWeek = new Date().getDay();
+// if Monday or Tuesday displays this
+if (dayOfWeek == 1 || dayOfWeek == 2) {
+    chamber.style.display = "block";
+}
+//Let's get the hamburguer working!
 
 function toggleMenu() {
-  document.getElementById('Nav').classList.toggle('open');
+
+    document.getElementById('Nav').classList.toggle('open');
 }
 
-let navig = document.getElementById('hamburgerBtn');
-navig.onclick = toggleMenu;
+let navig = document.getElementById('hamburgerBtn')
+navig.onclick = toggleMenu
 
-document.getElementById("modify").innerHTML = document.lastModified;
-document.getElementById("currentYear").textContent = new Date().getFullYear();
+document.getElementById("modify").innerHTML = document.lastModified
+document.getElementById("year").innerHTML = new Date().getFullYear()
+
+  
---  Bountiful Foods/script.js
+++  Bountiful Foods/script.js
@@ -3,4 +3,26 @@ function openMenu() {
     // Implement the openMenu function logic
     console.log("Menu opened");
   }
-  
+  "use strict";
+
+document.querySelector('#now').innerHTML = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format();
+//document.getElementById("displayDateTime").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
+
+let chamber = document.querySelector('#chamber');
+let dayOfWeek = new Date().getDay();
+// if Monday or Tuesday displays this
+if (dayOfWeek == 1 || dayOfWeek == 2) {
+    chamber.style.display = "block";
+}
+//Let's get the hamburguer working!
+
+function toggleMenu() {
+
+    document.getElementById('Nav').classList.toggle('open');
+}
+
+let navig = document.getElementById('hamburgerBtn')
+navig.onclick = toggleMenu
+
+document.getElementById("modify").innerHTML = document.lastModified
+document.getElementById("year").innerHTML = new Date().getFullYear()
