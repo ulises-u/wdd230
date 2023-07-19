fb5972971081bf2502a5cebfe480a20f94e9b832
ulises-u <ulisesmartir@byui.edu>
2023-07-18 21:45:34 -0500

    nav fixed

---  Bountifulfoods /js/script.js
+++  Bountifulfoods /js/script.js
@@ -8,11 +8,11 @@ function openMenu() {
 document.querySelector('#now').innerHTML = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format();
 //document.getElementById("displayDateTime").innerHTML = dateTime + ' <br> Day :- ' + daylist[day];
 
-let chamber = document.querySelector('#chamber');
+let Bountifulfoods = document.querySelector('#Bountifulfoods');
 let dayOfWeek = new Date().getDay();
 // if Monday or Tuesday displays this
 if (dayOfWeek == 1 || dayOfWeek == 2) {
-    chamber.style.display = "block";
+    Bountifulfoods.style.display = "block";
 }
 //Let's get the hamburguer working!
 
@@ -26,3 +26,5 @@ navig.onclick = toggleMenu
 
 document.getElementById("modify").innerHTML = document.lastModified
 document.getElementById("year").innerHTML = new Date().getFullYear()
+
+ 
