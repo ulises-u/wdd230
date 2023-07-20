07b20eb83acbf84ad395223e8f7e9511df210764
ulises-u <ulisesmartir@byui.edu>
2023-07-19 23:55:17 -0500

    dynamic spotlight

--- Chamber/js/Today.js
+++ Chamber/js/Today.js
@@ -21,3 +21,44 @@ navig.onclick = toggleMenu
 
 document.getElementById("modify").innerHTML = document.lastModified
 document.getElementById("year").innerHTML = new Date().getFullYear()
+//Let's get the Dinamic working!
+
+
+// Dynamic data for the spotlight section (events)
+const spotlightItems = [
+    {
+        title: "Event 1",
+        description: "Join us for a thrilling adventure in Event 1.",
+        time: "July 20, 2023 7:00 PM"
+    },
+    {
+        title: "Event 2",
+        description: "Experience the magic of Event 2 with spectacular performances.",
+        time: "August 5, 2023 8:30 PM"
+    },
+    // Add more events as needed
+];
+
+// Function to create dynamic spotlight items
+function createSpotlightItems() {
+    const spotlightContainer = document.getElementById("chamber");
+    let spotlightHTML = "";
+
+    // Loop through the spotlightItems array and generate HTML for each item
+    for (const item of spotlightItems) {
+        spotlightHTML += `
+            <div class="spotlight-item">
+                <h2>${item.title}</h2>
+                <p>${item.description}</p>
+                <p>${item.time}</p>
+            </div>
+        `;
+    }
+
+    // Update the spotlight container with the dynamic HTML
+    spotlightContainer.innerHTML = spotlightHTML;
+}
+
+// Call the function to create the spotlight items when the DOM is ready
+document.addEventListener("DOMContentLoaded", createSpotlightItems);
+
